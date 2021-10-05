8d17fb4b3e490796e9ef9e42dffe942542cf712c
28121987 <87053179+28121987@users.noreply.github.com>
2021-10-05 13:57:00 +0530

    Update firstjavaprogram.java

--- firstjavaprogram.java
+++ firstjavaprogram.java
@@ -2,6 +2,6 @@ public class firstjavaprogram
 {
 public static void main(String args[])
 {
-System.out.println("frist java program on Desktop");
+System.out.println("frist java program on Desktop on 5/10/2021");
 }
 }
