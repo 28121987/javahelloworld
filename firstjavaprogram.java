e1eefb00c1d87788a17078e4f3f0f70e28c759ee
pravin <pravin_patil@pvppcoe.ac.in>
2022-03-15 09:41:53 +0530

    commited by me on 15/3/2022

--- firstjavaprogram.java
+++ firstjavaprogram.java
@@ -3,5 +3,6 @@ public class firstjavaprogram
 public static void main(String args[])
 {
 System.out.println("frist java program on Desktop on 5/10/2021");
+System.out.println("just updated on 15/3/2022");
 }
 }
